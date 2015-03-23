7b942ee87f011233de5073d1b327d370e8f01a06
Wei Song <wsong83@gmail.com>
2015-03-23 10:02:39 +0000

    revise build_image.sh in zedboard, cross-compiler in tag tests, and the set_riscv_env to echo TOP dir

--- set_riscv_env.sh
+++ set_riscv_env.sh
@@ -1,6 +1,10 @@
+#!/bin/bash
 # source this file
 echo "Setting up RISC-V environment..."
 # Variables for RISC-V
-export TOP=$PWD
+if [ "$TOP" == "" ]; then
+    echo "\$TOP is not available. So set it to the current directory $PWD."
+    export TOP=$PWD
+fi
 export RISCV=$TOP/riscv
 export PATH=$PATH:$RISCV/bin
