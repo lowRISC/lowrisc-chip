50c3b4414651737a1e7e9c37ea32764fe4ebfb11
Stefan Wallentowitz <stefan@wallentowitz.de>
2016-03-11 14:49:29 +0100

    Update MAM

--- src/main/verilog/debug_system.sv
+++ src/main/verilog/debug_system.sv
@@ -172,6 +172,7 @@ module debug_system
        .MEM_SIZE(MAM_MEM_SIZE), .ADDR_WIDTH(MAM_ADDR_WIDTH),
        .MAX_PKT_LEN(MAX_PKT_LEN))
    u_mam (.*,
+          .req_beats (req_size),
           .id (10'd3),
           .debug_in        ( dii_in[3]        ),
           .debug_in_ready  ( dii_in_ready[3]  ),
