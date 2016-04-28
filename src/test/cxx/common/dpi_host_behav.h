e9a7eb4caa6c31c0e0f01f349aa1ede1a0068fe2
Wei Song <wsong83@gmail.com>
2016-04-28 15:43:47 +0100

    update behav_host for merged lowrisc-chip

--- src/test/cxx/common/dpi_host_behav.h
+++ src/test/cxx/common/dpi_host_behav.h
@@ -9,24 +9,11 @@
 extern "C" {
 #endif
 
-  extern void host_resp (unsigned int id, unsigned long long data);
+  // purely legacy code for ISA regression test
   extern void host_req (unsigned int id, unsigned long long data);
 
 #ifdef __cplusplus
 }
 #endif
-
-inline uint8_t host_extract_device(uint64_t data) {
-  return data >> 56;
-}
-
-inline uint8_t host_extract_cmd(uint64_t data) {
-  return data >> 48;
-}
-
-inline uint64_t host_extract_payload(uint64_t data) {
-  return data << 16 >>16;
-}
-
 
 #endif
