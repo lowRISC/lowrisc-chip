96a09f9c6f010577c10e32f90c4359323daecd47
Ciro Santilli 六四事件 法轮功 包卓轩 <ciro.santilli@gmail.com>
2016-06-28 12:40:01 +0200

    Replace [ == "" ] with [ -z ] set_riscv_env.sh
    `==` is a bash extensionand does the same as `=` here, so `=` is always preferred.
    
    But since we are at it, let's just use the more elegant `-z` for this special `""` case.

--- set_riscv_env.sh
+++ set_riscv_env.sh
@@ -3,7 +3,7 @@
 echo "Setting up lowRISC/RISC-V environment..."
 echo "Make sure you source this script at the top of lowrisc-chip."
 # Variables for lowRISC/RISC-V
-if [ "$TOP" == "" ]; then
+if [ -z "$TOP" ]; then
     echo "\$TOP is not available."
     echo "Set \$TOP to the top of lowrisc-chip which is the current directory."
     export TOP=$PWD
