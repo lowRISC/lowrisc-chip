b730765e64c7fc44b62f1f6b76fcf449f33cfadc
Wei Song <wsong83@gmail.com>
2016-07-06 16:43:49 +0100

    better print out for set_env.sh

--- set_env.sh
+++ set_env.sh
@@ -2,17 +2,16 @@
 # source this file
 echo "Setting up lowRISC/RISC-V and SoC Debug SoC environment..."
 echo "Make sure you source this script at the top of lowrisc-chip."
+echo
 
 # Variables for lowRISC/RISC-V
 if [ -z $TOP ] || [ ! -d $TOP ]; then
-    echo "\$TOP is not defined or does not point to a directory."
-    echo "Set \$TOP to the top of lowrisc-chip which is the current directory."
+    echo "\$TOP is not defined or does not point to a directory. Set \$TOP to the top of lowrisc-chip which is the current directory."
     export TOP=$PWD
 fi
 
 if [ -z $RISCV ]; then
-    echo "\$RISCV is not defined."
-    echo "Set \$TOP/riscv to the RISC-V toolchain installation target (\$RISCV)."
+    echo "\$RISCV is not defined. Set \$TOP/riscv to the RISC-V toolchain installation target (\$RISCV)."
     export RISCV=$TOP/riscv
 fi
 
@@ -41,8 +40,17 @@ fi
 
 # choose the FPGA board (Nexys4-DDR in default)
 if [ -z $FPGA_BOARD ]; then
-    echo "\$FPGA_BOARD is not defined."
-    echo "Set the target FPGA board to nexys4_ddr."
+    echo "\$FPGA_BOARD is not defined. Set the target FPGA board to nexys4_ddr."
     export FPGA_BOARD=nexys4_ddr
 fi
+
+echo "============================"
+echo "    export TOP=$TOP"
+echo "    export RISCV=$RISCV"
+echo "    export OSD_ROOT=$OSD_ROOT"
+echo "    export PATH=$PATH"
+echo "    export LD_LIBRARY_PATH=$LD_LIBRARY_PATH"
+echo "    export PKG_CONFIG_PATH=$PKG_CONFIG_PATH"
+echo "    export FPGA_BOARD=$FPGA_BOARD"
+echo "============================"
 
